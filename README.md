a2dd85631aee811b569be9cd515cef8e267aef06
Christoph, René Pardon <pardon@vimp.com>
2020-01-22 14:19:03 +0100

    feat (DEVOPS): adjust documentation and fix bash color codes because light green is not a perfect color. Especiall if it keeps like this after the last output

--- README.md
+++ README.md
@@ -3,19 +3,22 @@
 This bash script sets up a queue listener with supervisor on homestead in a breeze. It will listen for your queue's automatically when you run vagrant up.
 
 ## Installation
-From within your laravel project root ssh into homestead
+
+SSH into homestead first:
 
 ```bash
-vagrant ssh
+homestead ssh
 ```
 
 The second and last step is to run the script. It will ask the path to your project's root on homestead.
 
 ```bash
-source <(curl -s https://raw.githubusercontent.com/gmooren/laravel-homestead-queue-supervisor/master/homestead_queue_supervisor.sh)
+sudo su
+source <(curl -s https://raw.githubusercontent.com/renepardon/laravel-homestead-queue-supervisor/master/homestead_queue_supervisor.sh)
 ```
 
 ### Artisan queue command
+
 Supervisor will execute the following command:
 
 ```bash
@@ -25,8 +28,10 @@ php artisan --timeout=300 queue:listen
 You can add options to the command or change the command in `/usr/local/bin/run_queue.sh`
 
 ## Future work
+
 This bash script is very basic at the moment. Extending the script with options in order to specify things like timeouts, env, retry etc. is on the roadmap.
 
 ## License
 
 Laravel Homestead Queue Supervisor is open-sourced software licensed under the MIT license
+Original work done by @gmooren
--- homestead_queue_supervisor.sh
+++ homestead_queue_supervisor.sh
@@ -1,51 +1,52 @@
 #!/bin/bash
 
-
 echo -e "Install supervisor for Laravel queue worker"
-
-echo -e "\e[92m"
 
 while true; do
     read -p "What is the path to the laravel project's artisan file? (e.g. /home/vagrant/projectx): " path
 
     if [ ! -d $path ]; then
-        echo "\e[39mPath not found!"
+        echo "\e[1mPath not found!\e[21m"
     else
 
-        if [ ! -f /etc/supervisor/conf.d/laravel_queue.conf  ]; then
+        if [ ! -f /etc/supervisor/conf.d/laravel-queue-worker.conf  ]; then
 
-            echo "[program:laravel_queue]
-command=/usr/local/bin/run_queue.sh
+            echo "[program:laravel-queue-worker]
+process_name=%(program_name)s_%(process_num)02d
+command=/usr/local/bin/run-laravel-queue.sh
+user=vagrant
+numprocs=1
 autostart=true
 autorestart=true
 stderr_logfile=/var/log/laraqueue.err.log
-stdout_logfile=/var/log/laraqueue.out.log" > /etc/supervisor/conf.d/laravel_queue.conf
+stdout_logfile=/var/log/laraqueue.out.log" > /etc/supervisor/conf.d/laravel-queue-worker.conf
 
         fi
-        
-        if [ ! -f /usr/local/bin/run_queue.sh ]; then
 
+        if [ ! -f /usr/local/bin/run-laravel-queue.sh ]; then
+
+            # Remember to use queue:work on production for better performance!
             echo "#!/bin/bash
-php $path/artisan --timeout=240 queue:listen" > /usr/local/bin/run_queue.sh
+php $path/artisan --timeout=240 queue:listen" > /usr/local/bin/run-laravel-queue.sh
 
-            chmod +x /usr/local/bin/run_queue.sh
+            chmod +x /usr/local/bin/run-laravel-queue.sh
 
-            echo -e "\e[39mQueue worker will start automatically from now on"
+            echo -e "\e[1mQueue worker will start automatically from now on\e[21m"
             break
 
         else
 
-            if grep -q "$path" /usr/local/bin/run_queue.sh
+            if grep -q "$path" /usr/local/bin/run-laravel-queue.sh
                 then
-                    echo -e "\e[91mSupervisor queue already configured for the given path\e[39m"
+                    echo -e "\e[1mSupervisor queue already configured for the given path\e[21m"
             else
-                echo "php $path --timeout=300 queue:listen" >> /usr/local/bin/run_queue.sh
+                echo "php $path --timeout=300 queue:listen" >> /usr/local/bin/run-laravel-queue.sh
             fi
 
         fi
 
     fi
-    
+
   break
-    
+
 done
