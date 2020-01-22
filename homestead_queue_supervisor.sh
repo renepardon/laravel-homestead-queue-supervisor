6cbc0a10dcc7b714ba3f1fef43af1e3b1bd9539b
Christoph, René Pardon <pardon@vimp.com>
2020-01-22 14:30:25 +0100

    fix (DEVOPS): more maintainable way of bash text schema

--- homestead_queue_supervisor.sh
+++ homestead_queue_supervisor.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-echo -e "Install supervisor for Laravel queue worker"
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+echo -e "${bold}Install supervisor for Laravel queue worker${normal}"
 
 while true; do
-    read -p "What is the path to the laravel project's artisan file? (e.g. /home/vagrant/projectx): " path
+    read -p "${bold}What is the path to the laravel project's artisan file? (e.g. /home/vagrant/projectx): ${normal}" path
 
     if [ ! -d $path ]; then
-        echo "\e[1mPath not found!\e[21m"
+        echo -e "${bold}Path not found!${normal}"
     else
-
         if [ ! -f /etc/supervisor/conf.d/laravel-queue-worker.conf  ]; then
-
             echo "[program:laravel-queue-worker]
 process_name=%(program_name)s_%(process_num)02d
 command=/usr/local/bin/run-laravel-queue.sh
@@ -20,31 +21,24 @@ autostart=true
 autorestart=true
 stderr_logfile=/var/log/laraqueue.err.log
 stdout_logfile=/var/log/laraqueue.out.log" > /etc/supervisor/conf.d/laravel-queue-worker.conf
-
         fi
 
         if [ ! -f /usr/local/bin/run-laravel-queue.sh ]; then
-
             # Remember to use queue:work on production for better performance!
             echo "#!/bin/bash
 php $path/artisan --timeout=240 queue:listen" > /usr/local/bin/run-laravel-queue.sh
 
             chmod +x /usr/local/bin/run-laravel-queue.sh
 
-            echo -e "\e[1mQueue worker will start automatically from now on\e[21m"
+            echo -e "${bold}Queue worker will start automatically from now on!${normal}"
             break
-
         else
-
-            if grep -q "$path" /usr/local/bin/run-laravel-queue.sh
-                then
-                    echo -e "\e[1mSupervisor queue already configured for the given path\e[21m"
+            if grep -q "$path" /usr/local/bin/run-laravel-queue.sh; then
+                echo -e "${bold}Supervisor queue already configured for the given path${normal}"
             else
                 echo "php $path --timeout=300 queue:listen" >> /usr/local/bin/run-laravel-queue.sh
             fi
-
         fi
-
     fi
 
   break
